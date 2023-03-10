f4f74582d7ccbb93e0b6ce95bccee060a17f24a4
Marc Mata <marcmata26@gmail.com>
2023-03-10 10:57:32 -0600

    Added gitignore and almost completed breakout game. Have to add score display

--- js/breakout_index.js
+++ js/breakout_index.js
@@ -117,6 +117,29 @@ timerId =setInterval(moveBall, 30);
 
 //check for collisions
 function checkForCollisions(){
+    //check for block collisions
+    for (let i = 0; i < blocks.length; i++) {
+       if (
+           (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
+        ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1])
+    ) {
+        const allBlocks = Array.from(document.querySelectorAll('.block'));
+            allBlocks[i].classList.remove('block')
+           blocks.splice(i, 1)
+           changeDirection()
+           score++
+           scoreDisplay.innerHTML = score
+
+           //check for win
+           if (blocks.length === 0) {
+               scoreDisplay.innerHTML = 'YOU WIN!!!!!'
+               clearInterval(timerId)
+               document.removeEventListener('keydown', moveUser)
+           }
+        }
+    }
+
+
     //check for wall collisions
     if (
         ballCurrentPosition[0] >= (boardWidth - ballDiameter) ||
@@ -125,6 +148,15 @@ function checkForCollisions(){
     ){
         changeDirection();
     }
+
+    //check for user collisions
+    if (
+        (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth)
+        && (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight)
+    ) {
+        changeDirection()
+    }
+
     //check for game over
     if (ballCurrentPosition[1] <= 0) {
         clearInterval(timerId)
